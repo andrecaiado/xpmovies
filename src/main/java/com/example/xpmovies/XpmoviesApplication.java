490a865d9fd0079aaaf552c4b6617829108b12fe
André Caiado <andrecaiado@gmail.com>
2022-01-10 16:53:07 +0000

    Created spring boot project

---
+++ src/main/java/com/example/xpmovies/XpmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.xpmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpmoviesApplication.class, args);
+	}
+
+}
